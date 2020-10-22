07cf67fdfdd1967161ab127f34e1f60616cc42b2
ywj3493 <ywj3493@nate.com>
2020-10-22 16:37:36 +0900

    프로퍼티에 함수를 직접 넣는 것도 가능하다

--- src/Chapter01/Counter.js
+++ src/Chapter01/Counter.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function Counter() {
+function Counter(props) {
+    //초기값 설정은 useState 에서
+    const [number, setNumber] = useState(0);
+
+    const onIncrease = () => {
+        setNumber(prevNumber => prevNumber+1);
+    }
+
+    const onDecrease = () => {
+        setNumber(prevNumber => prevNumber-1);
+    }
+
+    const onSquare = () => {
+        setNumber(props.square);
+    }
+    
     return (
         <div>
-            <h0>0</h0>
-            <button>-1</button>
-            <button>+1</button>
+            <h0>{number}</h0>
+            <button onClick = {onDecrease}>-1</button>
+            <button onClick = {onIncrease}>+1</button>
+            {props.square && <button onClick = {onSquare}>^2</button>}
         </div>
     );
 }
--- src/stories/CounterStory.jsx
+++ src/stories/CounterStory.jsx
@@ -3,4 +3,4 @@ import { storiesOf } from '@storybook/react';
 
 import Counter from '../Chapter01/Counter';
 
-storiesOf('Counter', module).add('기본 설정', () => <Counter/>);
+storiesOf('Counter', module).add('기본 설정', () => <Counter/>).add('과연 이게 될까?', () => <Counter square={a=>a*a} />);
