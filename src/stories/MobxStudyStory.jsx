818ff308faae35ab75ef2f9a4cc5a7ac496c62e0
ywj3493 <ywj3493@nate.com>
2020-11-17 14:37:14 +0900

    mobx 실습

---
+++ src/stories/MobxStudyStory.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import MobxStudy from '../Mobx/MobxStudy';
+
+storiesOf('MobxStudy', module).add('기본 설정', () => <MobxStudy/>)
